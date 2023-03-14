6eba4049c7e472a6acc2db1a06a2509cf559518f
vusalrahimli1 <vusalrahimli999@gmail.com>
2023-03-14 12:55:38 +0400

    Some changes

--- app/src/main/java/com/example/moviesapp/data/other/Constants.kt
+++ app/src/main/java/com/example/moviesapp/data/other/Constants.kt
@@ -2,7 +2,7 @@ package com.example.moviesapp.data.other
 
 object Constants {
     const val DB_NAME = "movies_database"
-    const val API_KEY = "531e51cd16eae4c5d2cbe83c5043fba8"
+    const val API_KEY = "api_key"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/original"
 }
